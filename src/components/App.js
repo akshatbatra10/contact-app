221c060f7f44a44354e2db85ded83955d1f3bc68
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2022-10-30 14:42:27 +0530

    created a overview of the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+
+import Header from "./Header.component";
+import AddContact from "./AddContact.component";
+import ContactList from "./ContactList.component";
+
 import "./App.css";
 
 function App() {
-  return <div>Hello World!!</div>;
+  return (
+    <div className='ui container'>
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
